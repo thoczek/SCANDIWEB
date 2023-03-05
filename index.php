e2897e3ca8e1d772fbc6c3027f35673f560ed1b4
thoczek <thoczek@poczta.onet.pl>
2023-03-05 13:54:02 +0100

    Update index.php
    Router

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
+/**
+ * This is the Front Controller.
+ * The Front Controller decides which action to run.
+ *
+ * This particular Front Controller defines a route table, which says
+ * which defines which URLs map to which actions.
+ *
+* Front controller
+ */
 include_once("src/autoloader.php");
 
-$page = new cpage("test");
+// Define the routes table
+$routes = array(
+    '/\/hello\/(.+)/' => array('HelloController', 'helloAction')
+);
+
+// Decide which route to run
+foreach ($routes as $url => $execute) {
+
+    //Check request to mach defined routes
+    $matches = preg_match($url, $_SERVER['REQUEST_URI'], $params);
+
+    // If matches
+    if ($matches > 0) {
+
+        // Start controller with passed params
+        $controller = new $execute[0]; // Cmienić aby odpalał wszystko w konstruktorze
+        $controller->{$execute[1]}($params);
+
+        break;
+    }
+}
 ?>
