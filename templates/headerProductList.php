81de172c778c7bbb7278d069100132ce5b11c335
thoczek <thoczek@poczta.onet.pl>
2023-03-09 08:21:33 +0100

    View separation

---
+++ templates/headerProductList.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="../css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="../css/all.min.css" />
+    <title>Product List</title>
+      <meta
+      name="description"
+      content="................."
+    />
+    <meta
+      name="keywords"
+      content="....................................."
+    />
+    <meta name="robots" content="follow" />
+    <meta name="author" content="Tadeusz Hoczek" />
+    <meta name="copyright" content="Tadeusz Hoczek" />
+  </head>
+
+  <body>
